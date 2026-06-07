253031ba5be0d8820afaa566e1618fc49c5d87d6
Rudov Andrey <a.rudov@clickadu.com>
2026-06-07 07:46:23 +0200

    add logger init

--- cmd/HiveAPI/main.go
+++ cmd/HiveAPI/main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"HiveAPI/internal/config"
-	"log"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	log.Printf("config: %+v", cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting server")
+	log.Debug("debug logging enabled")
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+
+	return log
 }
